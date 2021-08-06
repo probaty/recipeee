cc71746482459ac766c2ca853ad40ac680477b95
Maxasss <travokur321@yandex.ru>
2021-08-06 21:39:24 +0500

    fix fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -26,9 +26,9 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Georama:wght@300;400;500;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Comfortaa&display=swap"
             rel="stylesheet"
-          />{" "}
+          />
         </Head>
         <body>
           <Main />
